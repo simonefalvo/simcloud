c1a1074fce15a6f2afc9d9ad2b289a4505e6ba4c
smvfal <smvfal@gmail.com>
2018-09-01 23:49:20 +0200

    controller, job replacing

--- msq.c
+++ msq.c
@@ -18,6 +18,7 @@
 #define START    0.0            /* initial (open the door) time   */
 #define STOP     20000.0        /* terminal (close the door) time */
 #define N        20             /* number of servers              */
+#define S        20             /* threshold                      */
 #define L1       4.0            /* class 1 arrival rate           */
 #define L2       6.25           /* class 2 arrival rate           */
 #define M1CLET   0.45           /* class 1 cloudlet service rate  */
@@ -25,7 +26,18 @@
 #define M1CLOUD  0.25           /* class 1 cloud service rate     */
 #define M2CLOUD  0.22           /* class 2 cloud service rate     */
 #define SETUP    0.8            /* class 2 mean setup time        */
+#define J_CLASS1 0
+#define J_CLASS2 1
+#define CLET     0
+#define CLOUD    1
+#define SRV_IDLE 0
+#define SRV_BUSY 1
 
+
+typedef struct {
+    double current;             /* current time                    */
+    double next;                /* next (most imminent) event time */
+} clock;
 
 typedef struct {                /* the next-event list    */
     double t;                   /*   next event time      */
@@ -33,6 +45,10 @@ typedef struct {                /* the next-event list    */
     int j;                      /*   job class type       */
 } event_list[N + 1];
 
+typedef struct {                /* accumulated sums of    */
+    double service;             /*   service times        */
+    long served;                /*   number served        */
+} srvstat_list[N + 1];
 
 double Exponential(double m)
 /* ---------------------------------------------------
@@ -120,45 +136,85 @@ int FindOne(event_list event)
     return (s);
 }
 
+int find_idle(event_list event)
+/* ---------------------------------------------------
+ * return the index of the first available idle server 
+ * ---------------------------------------------------
+ */
+{
+    int s = 1;
+    while (event[s].x == SRV_BUSY)     
+        s++;                          
+    return s;
+}
+
+int find_busy(event_list event, int jobclass)
+/* -----------------------------------------
+ * return the index of the first busy server 
+ * with a job of the specified class
+ * -----------------------------------------
+ */
+{
+    int s = 1;
+    while (event[s].x == SRV_BUSY && event[s].j != jobclass)
+        s++;
+    return s;
+}
+
+void srvjob(event_list event, srvstat_list sum, clock t)
+{
+    double service = GetService(event[0].j);
+    int s = find_idle(event);
+    sum[s].service += service;
+    sum[s].served++;
+    event[s].t = t.current + service;
+    event[s].x = SRV_BUSY;
+    event[s].j = event[0].j; 
+}
+
+void rplcjob(event_list event, srvstat_list sum, clock t)
+{
+    double service = GetService(event[0].j);
+    int s = find_busy(event, J_CLASS2);
+    sum[s].service -= event[s].t - t.current;
+    sum[s].service += service;
+    event[s].t = t.current + service;
+    event[s].j = event[0].j; 
+}
 
 int main(void)
 {
-    int j;
-
-    struct {
-        double current;         /* current time                       */
-        double next;            /* next (most imminent) event time    */
-    } t;
+    clock t;
     event_list event;
-    long number = 0;            /* number in the node                 */
-    int e;                      /* next event index                   */
-    int s;                      /* server index                       */
-    long arrived = 0;           /* arrived jobs counter               */
-    long index = 0;             /* processed jobs counter             */
-    long lost = 0;              /* lost jobs counter                  */
-    double area = 0.0;          /* time integrated number in the node */
-    struct {                    /* accumulated sums of                */
-        double service;         /*   service times                    */
-        long served;            /*   number served                    */
-    } sum[N + 1];
+    srvstat_list sum;
+
+    int jobclass;
+    long n1 = 0;             /* number of class 1 job in the cloudlet */
+    long n2 = 0;             /* number of class 2 job in the cloudlet */
+    int e;                   /* next event index                      */
+    int s;                   /* server index                          */
+    long arrived = 0;        /* arrived jobs counter                  */
+    long processed = 0;      /* processed jobs counter                */
+    long lost = 0;           /* lost jobs counter                     */
+    double area = 0.0;       /* time integrated number in the node    */
 
     PlantSeeds(0);
     t.current = START;
-    event[0].t = GetArrival(&j);
-    event[0].x = 1;
-    event[0].j = j;
+    event[0].t = GetArrival(&jobclass);
+    event[0].x = SRV_BUSY;
+    event[0].j = jobclass;
 
     for (s = 1; s <= N; s++) {
-        event[s].t = START;     /* this value is arbitrary because */
-        event[s].x = 0;         /* all servers are initially idle  */
+        event[s].t = START;         /* this value is arbitrary because */
+        event[s].x = SRV_IDLE;      /* all servers are initially idle  */
         sum[s].service = 0.0;
         sum[s].served = 0;
     }
 
-    while ((event[0].x != 0) || (number != 0)) {
+    while ((event[0].x != 0) || (n1 + n2 != 0)) {
         e = NextEvent(event);   /* next event index */
         t.next = event[e].t;    /* next event time  */
-        area += (t.next - t.current) * number;  /* update integral  */
+        area += (t.next - t.current) * (n1 + n2);  /* update integral  */
         t.current = t.next;     /* advance the clock */
 
         if (e == 0) {           /* process an arrival */
@@ -166,57 +222,69 @@ int main(void)
             if (event[0].t > STOP)
                 event[0].x = 0; /* turn off the event */
 
-            if (number < N) {
-                number++; 
-                double service = GetService(event[0].j);
-                s = FindOne(event);
-                sum[s].service += service;
-                sum[s].served++;
-                event[s].t = t.current + service;
-                event[s].x = 1;
-                event[s].j = j; 
+            if (event[0].j == J_CLASS1) { /* process a class 1 arrival */
+                if (n1 == N) 
+                    lost++;
+                else if (n1 + n2 < S) { // accept job
+                        srvjob(event, sum, t);
+                        n1++;
+                    }
+                    else if (n2 > 0) { // replace a class 2 job 
+                            rplcjob(event, sum, t);
+                            n1++;
+                            n2--;
+                            lost++;
+                        }
+                        else { // accept job
+                            srvjob(event, sum, t);
+                            n1++;
+                        }
             }
-            else lost++;
+            else {                 /* process a class 2 arrival */
+                if (n1 + n2 > S)
+                    lost++;
+                else {
+                    srvjob(event, sum, t);
+                    n2++;
+                }
+            }
 
-            event[0].t = GetArrival(&j);  /* set next arrival t */
-            event[0].j = j;               /* set next arrival j */
+            event[0].t = GetArrival(&jobclass);  /* set next arrival t */
+            event[0].j = jobclass;               /* set next arrival j */
 
         } else {                /* process a departure */
-            index++;            
-            number--;
             s = e;
-            event[s].x = 0;
-            /*
-            if (number >= N) {
-                double service = GetService(event[0].j);
-                sum[s].service += service;
-                sum[s].served++;
-                event[s].t = t.current + service;
-            } else
-                event[s].x = 0;
-            */
+            event[s].x = SRV_IDLE;
+            if (event[s].j == J_CLASS1)
+                n1--;
+            else 
+                n2--;
+            processed++;            
         }
     }
 
     printf("\nArrived jobs: %ld\n", arrived);
-    printf("Processed jobs: %ld\n", index);
+    printf("Processed jobs: %ld\n", processed);
     printf("Lost jobs: %ld\n", lost);
-    printf("for %ld jobs the service node statistics are:\n\n", index);
-    printf("  avg interarrivals .. = %6.2f\n", event[0].t / index);
-    printf("  avg wait ........... = %6.2f\n", area / index);
+    printf("Lost + processed: %ld\n", lost + processed);
+
+
+    printf("for %ld jobs the service node statistics are:\n\n", processed);
+    printf("  avg interarrivals .. = %6.2f\n", event[0].t / arrived);
+    printf("  avg wait ........... = %6.2f\n", area / processed);
     printf("  avg # in node ...... = %6.2f\n", area / t.current);
 
     for (s = 1; s <= N; s++)    /* adjust area to calculate */
         area -= sum[s].service; /* averages for the queue   */
 
-    printf("  avg delay .......... = %6.2f\n", area / index);
+    printf("  avg delay .......... = %6.2f\n", area / processed);
     printf("  avg # in queue ..... = %6.2f\n", area / t.current);
     printf("\nthe server statistics are:\n\n");
     printf("    server     utilization     avg service        share\n");
     for (s = 1; s <= N; s++)
         printf("%8d %14.3f %15.2f %15.3f\n", s, sum[s].service / t.current,
                sum[s].service / sum[s].served,
-               (double) sum[s].served / index);
+               (double) sum[s].served / processed);
     printf("\n");
 
     return (0);
